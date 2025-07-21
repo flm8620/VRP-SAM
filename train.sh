f136c60309e1c11b3ea2c8b22f74492eebbde11a
Leman FENG <leman.feng@irootech.com>
2025-07-22 00:27:43 +0800

    update script

--- train.sh
+++ train.sh
@@ -2,11 +2,13 @@
 # sleep 6h
 PARTITION=Segmentation
 
-GPU_ID=0,1,2,3
+GPU_ID=4,5,6,7
 
-CUDA_VISIBLE_DEVICES=${GPU_ID} python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=6224 train.py \
+CUDA_VISIBLE_DEVICES=${GPU_ID} torchrun --nproc_per_node=4 --master_port=6224 train.py \
                         --epochs 50 \
                         --condition mask \
                         --lr 1e-4 \
                         --fold 0 \
-                        --logpath trn1_coco_mask_fold0
+                        --logpath trn1_coco_mask_fold0-resnet101 \
+                        --backbone resnet101 \
+                        --datapath /home/leman.feng/VRP-SAM-eval/datasets
